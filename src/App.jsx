6674421e193143f679a88442705457603fe4a98f
MarceloLima11 <marcelolima2021@gmail.com>
2023-10-13 11:07:57 -0300

    feat(*): structuring project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from './components/pages/Home'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+import NewProject from './components/pages/NewProject'
+
 function App() {
   return (
-    <>
-      <p>BudgetWise</p>
-    </>
+    <Router>
+      <ul>
+        <li>Home</li>
+        <li>Contato</li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/company" element={<Company />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/newproject" element={<NewProject />} />
+      </Routes>
+      <p>Footer</p>
+    </Router>
   )
 }
 
